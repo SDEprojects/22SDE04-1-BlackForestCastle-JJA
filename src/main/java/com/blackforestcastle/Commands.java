97c8791ffd99fdda6469566f81d43f88255cc4d5
JuanCarlos Jimenez <jcj237@outlook.com>
2022-08-04 13:39:30 -0700

    Adds ability to view previous scores AB#823

--- src/main/java/com/blackforestcastle/Commands.java
+++ src/main/java/com/blackforestcastle/Commands.java
@@ -109,6 +109,9 @@ public class Commands
                 break;
             case "use":
                 use(noun);
+                break;
+            case "scores":
+                GameInfo.viewScores();
         }
     }
 
