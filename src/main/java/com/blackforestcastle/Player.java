dba0142cfdad031b5825874e260f1e926e911c5e
JuanCarlos Jimenez <jcj237@outlook.com>
2022-08-11 12:22:42 -0700

    Merge branch 'jc-jfr' of /Users/juanca/Downloads/Complete Materials Set contents/Complete Materials Set contents/StudentWork/DSA/Black-Forest-Castle-v2 with conflicts.

--- src/main/java/com/blackforestcastle/Player.java
+++ src/main/java/com/blackforestcastle/Player.java
@@ -1,5 +1,6 @@
 package com.blackforestcastle;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,7 +28,7 @@ class Player extends Character {
         getPlayerAttackPower();
         int damageDone = getAttackPower() + randomNumber(10,0);
         npc.setHP(npc.getHP() - damageDone);
-        UI.textPrint("You did " + damageDone + " damage. The enemies health now is " + npc.getHP());
+        System.out.println("You did " + damageDone + " damage. The enemies health now is " + npc.getHP());
     }
 
     // helper for attack method
@@ -55,7 +56,7 @@ class Player extends Character {
         for (Item item : super.getInventory()) {
             System.out.print(item.getName() + ", ");
         }
-        UI.textPrint("");
+        System.out.println();
     }
 
     public Item checkInventoryForItem(String item) {
--- src/main/java/com/blackforestcastle/Room.java
+++ src/main/java/com/blackforestcastle/Room.java
@@ -3,6 +3,7 @@ package com.blackforestcastle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import com.blackforestcastle.Commands;
 
 public class Room {
 
